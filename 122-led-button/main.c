f8aa3300b67010a9271036e20c885744e9cc3980
Захар Чегодаев <zarhitec@mail.ru>
2026-09-15 13:12:09 +0300

    Сделана п1.2.2

---
+++ 122-led-button/main.c
@@ -0,0 +1,36 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/regs/addressmap.h"
+#include "hardware/regs/sio.h"
+// добавляем заголовочный файл функций работы с GPIO
+
+const uint BUTTON_PIN = 15;
+const uint LED_PIN = 25;
+
+int main()
+{
+   // инициализируем пин светодиода
+   gpio_init(LED_PIN);
+   gpio_set_dir(LED_PIN, GPIO_OUT);
+   // настраиваем пин светодиода на выход
+
+   gpio_init(BUTTON_PIN);
+   gpio_set_dir(BUTTON_PIN, GPIO_IN);
+   gpio_pull_up(BUTTON_PIN);
+
+   bool led = false;
+   bool previous = false;
+
+   while (1)
+   {
+       bool current = gpio_get(BUTTON_PIN);
+
+       if (previous == true && current == false)
+       {
+           led = !led;
+           gpio_put(LED_PIN, led);
+       }
+
+       previous = current;
+   }
+}

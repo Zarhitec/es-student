28ba0f1b176dc509a9cb8c484221d211719d6aec
Захар Чегодаев <zarhitec@mail.ru>
2026-09-17 22:12:45 +0300

    Сделана п1.3.4

--- 133-led-button-usb/main.c
+++ 133-led-button-usb/main.c
@@ -7,17 +7,38 @@
 const uint LED_PIN = 25;
 const uint BUTTON_PIN = 15;
 int DEBOUNCE_MS = 20;
-void set_led(bool on)
-{
-    gpio_put(LED_PIN, on);
-    printf("led %s\n", on ? "on" : "off");
-}
 
 bool get_button_debounce(uint pin)
 {
     bool state = gpio_get(pin);
     sleep_ms(DEBOUNCE_MS);
     return state && gpio_get(pin);
+}
+
+void set_led(bool on)
+{
+    gpio_put(LED_PIN, on);
+    printf("led %s\n", on ? "on" : "off");
+}
+
+bool handle_command(int command, bool led)
+{
+    if (command == 'e')
+    {
+        led = true;
+        set_led(led);
+    }
+    else if (command == 'd')
+    {
+        led = false;
+        set_led(led);
+    }
+    else
+    {
+        printf("unknown command: %c\n", command);
+    }
+
+    return led;
 }
 
 int main()
@@ -39,6 +60,15 @@ int main()
             set_led(led_state);
             previos_led_state = led_state;
         }
+
+        int command = getchar_timeout_us(0);
+
+        if (command == PICO_ERROR_TIMEOUT)
+        {
+            continue;
+        }
+
+        led_state = handle_command(command, led_state);
         
     }
 }
